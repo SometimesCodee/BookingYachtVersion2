4de98da741430120ab5d288ec863ca9d60a020b5
SometimesCodee <143207239+SometimesCodee@users.noreply.github.com>
2024-07-19 16:06:23 +0700

    Update BookNowModal.js

--- front_end/bookingyacht/src/components/detailYacht/priceRoom/BookNowModal.js
+++ front_end/bookingyacht/src/components/detailYacht/priceRoom/BookNowModal.js
@@ -17,7 +17,7 @@ const BookNowModal = ({
     handleClose,
     selectedSchedule
 }) => {
-    const getAvatarApi = `https://booking18-fzc0ghgvcve8f7fs.eastasia-01.azurewebsites.net/api/customer/file/`;
+ 
     const isLogged = useSelector(state => state.account.isAuthenticated);
     console.log('isLogged', isLogged)
     const [customer, setCustomer] = useState(null);

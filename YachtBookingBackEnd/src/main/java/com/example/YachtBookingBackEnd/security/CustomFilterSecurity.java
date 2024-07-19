1dd51b3e7a8f1e8e1cd13491fca9b5e85ffd8aa4
SometimesCodee <143207239+SometimesCodee@users.noreply.github.com>
2024-07-19 16:47:07 +0700

    Update CustomFilterSecurity.java

--- YachtBookingBackEnd/src/main/java/com/example/YachtBookingBackEnd/security/CustomFilterSecurity.java
+++ YachtBookingBackEnd/src/main/java/com/example/YachtBookingBackEnd/security/CustomFilterSecurity.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("*")
+@CrossOrigin("https://yb.sh.io.vn")
 @Configuration
 @EnableWebSecurity
 public class CustomFilterSecurity {
